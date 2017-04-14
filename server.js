9d7b05d3246fb6c3535021e7bd4fc13ec29bc4ad
PaulEmile <paulemile.chantrel@gmail.com>
2017-04-14 00:08:33 +0000

    seconde commit

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 var express = require('express');
 var app = express();
-app.get('/:id', function (req, res) {
-  
+app.get('/', function (req, res) {
+  res.send(RHP(req.get("User-Agent"),req.header('x-forwarded-for'),req.headers['accept-language']));
 });
 
 app.listen(8080);
+
+function RHP (ua,ip,lan){
+    var bracket1=false;
+    var soft="";
+    for (var i=0; i<ua.length;i++){
+        if (bracket1){
+            soft+=ua[i];
+        }
+        if(ua[i]=='('){
+            bracket1=true;
+        }
+        if (ua[i+1]==")"){
+            bracket1=false;
+            break;
+        }
+    }
+    var jsonData={ipAdress:ip,language:lan,software:soft};
+    return jsonData;
+}
